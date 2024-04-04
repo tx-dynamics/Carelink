87f54b9546948cf5b171fc3241267a8c79edcafd
usamatx12 <usama.asghar@txdynamics.io>
2024-04-04 12:26:11 +0500

    Adding firebase for fcm token

--- CareLink/src/Services/HelpingMethods.js
+++ CareLink/src/Services/HelpingMethods.js
@@ -1,56 +1,73 @@
-import ImageCropPicker from "react-native-image-crop-picker"
-import { heightPixel, widthPixel } from "../Constants"
+import ImageCropPicker from 'react-native-image-crop-picker';
+import {heightPixel, widthPixel} from '../Constants';
 
 export const uploadmageMultiPle = (setPicData, picData) => {
-    let temp = [...picData]
-    temp.pop()
-    ImageCropPicker.openPicker({
-        width: widthPixel(140),
-        height: widthPixel(140),
-        cropping: true
-    }).then(
-        // image => setPicData([...picData, { image: image?.path, add: false }]))
-        // image => setPicData([...picData, { image: image?.path, add: false }]))
-        image => {
-            temp.push({ image: image?.path, add: false })
-            temp.push({ image: "", add: true })
-            setPicData(temp)
-        })
-        .catch(err => console.log(err.message))
-}
+  let temp = [...picData];
+  temp.pop();
+  ImageCropPicker.openPicker({
+    width: widthPixel(140),
+    height: widthPixel(140),
+    cropping: true,
+  })
+    .then(
+      // image => setPicData([...picData, { image: image?.path, add: false }]))
+      // image => setPicData([...picData, { image: image?.path, add: false }]))
+      image => {
+        temp.push({image: image?.path, add: false});
+        temp.push({image: '', add: true});
+        setPicData(temp);
+      },
+    )
+    .catch(err => console.log(err.message));
+};
 export const removePic = (index, setPicData, picData) => {
-    picData.splice(index, 1)
-    setPicData([...picData])
-    console.log(picData.length)
-}
+  picData.splice(index, 1);
+  setPicData([...picData]);
+  console.log(picData.length);
+};
 export const uploadmageState = (setPic, setUpload, setVisible, height) => {
-    ImageCropPicker.openPicker({
-        width: widthPixel(414),
-        height: height ? height : heightPixel(360),
-        cropping: true
-    }).then(image => {
-        setUpload(true)
-        setVisible(false)
-        setPic(image?.path)
-    }).catch(e => console.log(e));
-}
+  ImageCropPicker.openPicker({
+    width: widthPixel(414),
+    height: height ? height : heightPixel(360),
+    cropping: true,
+  })
+    .then(image => {
+      setUpload(true);
+      setVisible(false);
+      setPic(image?.path);
+    })
+    .catch(e => console.log(e));
+};
 export const uploadmageCamState = (setPic, setUpload, setVisible, height) => {
-    ImageCropPicker.openCamera({
-        width: widthPixel(414),
-        height: height ? height : heightPixel(360),
-        cropping: true
-    }).then(image => {
-        setUpload(true)
-        setVisible(false)
-        setPic(image?.path)
-    }).catch(e => console.log(e));
-}
-export const uploadmage = (setPic) => {
-    ImageCropPicker.openPicker({
-        width: 600,
-        height: 600,
-        cropping: true,
-    }).then(image => {
-        setPic(image?.path)
-    }).catch(e => console.log(e));
-}
+  ImageCropPicker.openCamera({
+    width: widthPixel(414),
+    height: height ? height : heightPixel(360),
+    cropping: true,
+  })
+    .then(image => {
+      setUpload(true);
+      setVisible(false);
+      setPic(image?.path);
+    })
+    .catch(e => console.log(e));
+};
+export const uploadmage = setPic => {
+  ImageCropPicker.openPicker({
+    width: 600,
+    height: 600,
+    cropping: true,
+  })
+    .then(image => {
+      setPic(image?.path);
+    })
+    .catch(e => console.log(e));
+};
+
+export const getFCMToken = async () => {
+  const fcmToken = await messaging().getToken();
+  if (fcmToken) {
+    return fcmToken;
+  } else {
+    return null;
+  }
+};
